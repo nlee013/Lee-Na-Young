fa5742868360c2a4734985412f294d65028e04a5
Lee-Han-Sol <107240538+Lee-Han-Sol@users.noreply.github.com>
2022-08-02 18:49:25 +0900

    FIX : RENAME

--- src/main/java/com/springboot/lookoutside/controller/UserController.java
+++ src/main/java/com/springboot/lookoutside/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.spring.boot.controller;
+package com.springboot.lookoutside.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/test/java/com/springboot/lookoutside/LookOutsideApplicationTests.java
+++ src/test/java/com/springboot/lookoutside/LookOutsideApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spring.boot;
+package com.springboot.lookoutside;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
