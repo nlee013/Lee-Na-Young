65382b99d23aff2b9139a733e16cb79cf87aaad3
Lee-Han-Sol <107240538+Lee-Han-Sol@users.noreply.github.com>
2022-08-10 02:15:52 +0900

    FEAT : 회원권한수정, 비밀번호 재설정

--- src/main/java/com/springboot/lookoutside/Service/UserService.java
+++ src/main/java/com/springboot/lookoutside/Service/UserService.java
@@ -89,6 +89,30 @@ public class UserService {
 	
 	//회원정보수정
 	@Transactional
+	public void changeRole(String useId) {
+		//수정시에는 영속성 컨텍스트 User 오브젝트를 영속화시키고, 영속화된 User 오브젝트를 수정
+		// select를 해서 User오브젝트를 DB로 부터 가져오는 이유는 영속화를 하기위함
+		// 영속화된 오브젝트를 변경하면 자동으로 DB에 update문 실행
+		//User persistance = userRepository.findByUseId(user.getUseId()).orElseThrow(() -> { //user.getUserId -> 세션에 올라와있는 Id이용
+		User persistance = userRepository.findByUseId(useId).orElseThrow(() -> { //테스트용
+			return new IllegalArgumentException("회원찾기 실패");
+		});
+		
+		//권한 변경
+		if(persistance.getUseRole().equals("USER")) {
+			persistance.setUseRole("ADMIN");
+		}
+		
+		else {
+			persistance.setUseRole("USER");
+		}
+		
+		//회원정보 함수 종료시 서비스 종료 트랜잭션 종료 commit이 자동으로 실행
+		//persistance가 변화되면 자동으로 update문 실행
+	}
+	
+	//회원정보수정
+	@Transactional
 	public void updateUser(User user) {
 		//수정시에는 영속성 컨텍스트 User 오브젝트를 영속화시키고, 영속화된 User 오브젝트를 수정
 		// select를 해서 User오브젝트를 DB로 부터 가져오는 이유는 영속화를 하기위함
@@ -107,6 +131,26 @@ public class UserService {
 		
 		//닉네임 수정
 		persistance.setUseNick(user.getUseNick());
+		
+		//회원정보 함수 종료시 서비스 종료 트랜잭션 종료 commit이 자동으로 실행
+		//persistance가 변화되면 자동으로 update문 실행
+	}
+	
+	//비밀번호 재설정
+	@Transactional
+	public void newPw(User user, String useId) {
+		//수정시에는 영속성 컨텍스트 User 오브젝트를 영속화시키고, 영속화된 User 오브젝트를 수정
+		// select를 해서 User오브젝트를 DB로 부터 가져오는 이유는 영속화를 하기위함
+		// 영속화된 오브젝트를 변경하면 자동으로 DB에 update문 실행
+		
+		User persistance = userRepository.findByUseId(useId).orElseThrow(() -> { //테스트용
+			return new IllegalArgumentException("회원찾기 실패");
+		});
+		
+		//비밀번호 수정
+		String rawPassword = user.getUsePw();
+		String encPassword = encoder.encode(rawPassword);
+		persistance.setUsePw(encPassword);
 		
 		//회원정보 함수 종료시 서비스 종료 트랜잭션 종료 commit이 자동으로 실행
 		//persistance가 변화되면 자동으로 update문 실행
