6bacdb4513fa35911a60d71f28fda76adc38b393
Lee-Han-Sol <107240538+Lee-Han-Sol@users.noreply.github.com>
2022-08-10 23:02:59 +0900

    CHORE : 빌드를 위한 yml수정, REFACOTR : 컨트롤러 분리

--- src/main/java/com/springboot/lookoutside/config/SecurityConfig.java
+++ src/main/java/com/springboot/lookoutside/config/SecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.springboot.lookoutside.config.auth.PrincipalDetailService;
 
@@ -35,20 +38,38 @@ public class SecurityConfig {
 	@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+	        .httpBasic().disable()
+	        .cors().configurationSource(corsConfigurationSource())
+	        .and()
         	.csrf().disable() // csrf 토큰 비활성화 (테스트시 비활성화)
 	        .authorizeRequests()
-				.antMatchers("/user/**").permitAll()// user로 들어오는 경로 모두 허용
-				.antMatchers("/manager/**").hasRole("ADMIN") // Admin만 가능
+				.antMatchers("/","/user/**","/manager/**").permitAll()// user로 들어오는 경로 모두 허용
+				//.antMatchers("/manager/**").hasRole("ADMIN") // Admin만 가능
 				.anyRequest().authenticated() // 다른 요청은 인증이 되어야한다.
         	.and()
         		.formLogin()
+        		.successHandler(new CustomAuthenticationSuccessHandler())
         		 // .loginPage("/login") //로그인페이지 주소
-        		.loginProcessingUrl("/user/sign-in") // 스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인 해준다.
-        		.defaultSuccessUrl("/"); //성공시 데려갈 주소
+        		.loginProcessingUrl("/user/sign-in"); // 스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인 해준다.
+//        		.defaultSuccessUrl("/"); //성공시 데려갈 주소
+        		
         
         		
         return http.build();
     }
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
 
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 }
