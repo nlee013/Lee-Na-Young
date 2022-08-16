a46680f35d2d01c5fe88bd88d524fcf25c93d831
nlee013 <83215027+nlee013@users.noreply.github.com>
2022-08-17 03:04:20 +0900

    UPDATE : 댓글 작성, 수정, 삭제, 목록

--- src/main/java/com/springboot/lookoutside/Service/ArticleReplyService.java
+++ src/main/java/com/springboot/lookoutside/Service/ArticleReplyService.java
@@ -1,14 +1,12 @@
 package com.springboot.lookoutside.service;
 
-import java.util.Optional;
+import java.util.List;
 
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.springboot.lookoutside.domain.Article;
 import com.springboot.lookoutside.domain.ArticleReply;
 import com.springboot.lookoutside.repository.ArticleReplyRepository;
-
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
@@ -18,24 +16,46 @@ public class ArticleReplyService {
 	@Autowired
 	private ArticleReplyRepository articleReplyRepository;
 
-	public ArticleReply replyUpdate(int replyNo) {
+	//댓긇 등록
+	@Transactional
+	public String saveReply(ArticleReply articleReply) {
 		
-		return null;
+		articleReplyRepository.save(articleReply);
+		
+		return "1";
 	}
 
-	public ArticleReply replyDelete(int replyNo) {
+	//댓글 수정
+	public String updateReply(int repNo, ArticleReply articleReply) {
 		
-		return null;
+		articleReply =  articleReplyRepository.findByRepNo(repNo).orElseThrow(() -> { 
+			
+			return new IllegalArgumentException("0");
+		});
+		
+		articleReply.setRepContents(articleReply.getRepContents());
+		
+		return "1";
 	}
 
-	public void replySave(Article findArticle, String repContents) {
-		// TODO Auto-generated method stub
+	//댓글 삭제
+	public String deleteReply(int repNo) {
 		
+		articleReplyRepository.findByRepNo(repNo).orElseThrow(() -> { 
+			return new IllegalArgumentException("0");
+		});
+
+		articleReplyRepository.deleteById(repNo);
+		
+		return "1";
 	}
 
-	public ArticleReply replyList(int replyNo) {
-		// TODO Auto-generated method stub
-		return null;
+	//댓글 목록
+	public List<ArticleReply> replyList(int repNo, int artNo, ArticleReply articleReply) {
+		
+		List<ArticleReply> replyList = articleReplyRepository.findAllByArtNo(artNo);
+		
+		return replyList;
 	}
 	
 	
