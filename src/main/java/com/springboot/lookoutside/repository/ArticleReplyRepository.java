8e5752f9d7d62eb1935aea9e3980aac97529a8d9
nlee013 <83215027+nlee013@users.noreply.github.com>
2022-08-17 02:44:59 +0900

    UPDATE : 댓글 작성, 수정, 삭제, 목록

--- src/main/java/com/springboot/lookoutside/repository/ArticleReplyRepository.java
+++ src/main/java/com/springboot/lookoutside/repository/ArticleReplyRepository.java
@@ -1,9 +1,11 @@
 package com.springboot.lookoutside.repository;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springboot.lookoutside.domain.ArticleReply;
@@ -12,4 +14,8 @@ import com.springboot.lookoutside.domain.ArticleReply;
 public interface ArticleReplyRepository extends JpaRepository<ArticleReply, Integer> {
 
 	Optional<ArticleReply> findByRepNo(int repNo);
+	
+	//게시물에 대한 댓글 조회
+	List<ArticleReply> findAllByArtNo(int artNo);
+	
 }
